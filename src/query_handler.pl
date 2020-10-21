45cf9279dfd6bf32a7eb1bf41c9c0bf85f5c0441
Sascha Jongebloed <sasjonge@uni-bremen.de>
2020-10-21 14:29:50 +0200

    Moved handling for is_event to other package

--- src/query_handler.pl
+++ src/query_handler.pl
@@ -1,8 +1,9 @@
 :- module(query_handler,
     [ 
-    	openease_query(t,r),
-    	gen_msgs(t)
+    	openease_query(t,r)
     ]).
+
+:- dynamic gen_msgs/1.
 
 %:- use_module(library(data_vis)).
 
@@ -23,23 +24,3 @@
 openease_query(Query, Mode) :-
     call(Query),
     ignore(gen_msgs(Query)).
-
-get_event_data(E,Task,Start,End) :-
-	ask(aggregate([
-				triple(E,rdf:type,dul:'Event'),
-				triple(E,rdf:type,regex('^.*(?!Action).*')),
-				triple(E,dul:isClassifiedBy,TaskInstance),
-				triple(E,dul:hasTimeInterval,Interval),
-				triple(Interval,soma:hasIntervalBegin,Start),
-				triple(Interval,soma:hasIntervalEnd,End)
-			])),
-	atomic_list_concat([Task,_],'_',TaskInstance).
-
-gen_msgs(is_event(_)) :-
-    findall([E,Task,Start,End],(get_event_data(E,Task,Start,End)),EventData),
-    data_vis:timeline_data(EventData),
-    %%
-    findall(T0,member([_,_,T0,_],EventData),T0s),
-    member(Time,T0s),
-    marker_plugin:show_markers(Time).
-
